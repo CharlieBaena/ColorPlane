e056ac81deef743cdd5d91862b1d75192d1690cb
Francisco Moreno Castillo <56913146+SakHyen@users.noreply.github.com>
2020-02-16 22:43:31 +0100

    Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Text highscoreText;
+
+    void Start()	
+    {
+        highscoreText.text = "Highscore: " + PlayerPrefs.GetInt("highscore");
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
+}
